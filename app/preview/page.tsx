75e63df105593059f77fad8430a14bd744362456
suryanshsingh2001 <ricochetthestoryteller2001@gmail.com>
2024-12-08 03:45:12 +0530

    (Fix) Deployment Errors

--- app/preview/page.tsx
+++ app/preview/page.tsx
@@ -2,13 +2,10 @@
 
 import FormPreview from "@/components/form-section/FormPreview";
 import PreviewHeader from "@/components/layout/PreviewHeader";
-import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { useFormContext } from "@/context/FormContext";
-import { Check } from "lucide-react";
 
 export default function PreviewPage() {
-  const { isPreview, setIsPreview } = useFormContext();
 
   return (
     <Card className="container flex flex-col min-h-screen mx-auto max-w-3xl rounded-none">
