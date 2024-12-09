3558ee7ff091f45947f7fb31fd8b68806e63a99c
suryanshsingh2001 <ricochetthestoryteller2001@gmail.com>
2024-12-09 08:59:04 +0530

    (Fix) Layout issues in Submission Page

--- app/submissions/page.tsx
+++ app/submissions/page.tsx
@@ -6,7 +6,7 @@ import { useFormContext } from "@/context/FormContext";
 export default function FormSubmissionsPage() {
   const { formSubmissions } = useFormContext();
   return (
-    <div className="container mx-auto py-6 px-4 sm:px-6 lg:px-8">
+    <div className="container mx-auto py-6 px-4 sm:px-6 lg:px-8  max-w-4xl">
       <h1 className="text-2xl font-bold mb-6">Form Submissions</h1>
       <FormSubmissionsTable submissions={formSubmissions} />
     </div>
